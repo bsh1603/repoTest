91ebb7a9a7c7d4e1924c485005d43064f44372fb
hustlekang <rkdgusrn9688@gmail.com>
2022-11-24 17:02:15 +0900

    Feat: Login, Register

--- testing/src/main/frontend/src/App.js
+++ testing/src/main/frontend/src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import RegisterManager from "./pages/RegisterManager";
+import RegisterWorker from "./pages/RegisterWorker";
+
 function App() {
-  return <div className="App">init root</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>루트페이지</div>} />
+        <Route path="/registermanager" element={<RegisterManager />} />
+        <Route path="/registerworker" element={<RegisterWorker />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
